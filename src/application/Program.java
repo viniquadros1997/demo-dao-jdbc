79ab978f94e27f486e54f7762d1f1dbe59ca43d0
viniquadros1997 <viniquadros1997@gmail.com>
2021-06-06 23:07:38 -0300

    DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 2000.0, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		
 		
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -5,15 +5,12 @@ import java.util.List;
 import model.entities.Seller;
 
 public interface SellerDao {
-	
-	public interface DepartmentDao {
-		
-		void insert(Seller obj);
-		void update(Seller obj);
-		void deleteById(Integer id);
-		Seller findById(Integer id);
-		List<Seller> findALL();
-	}
-
-
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findALL();
 }
+
+
+
