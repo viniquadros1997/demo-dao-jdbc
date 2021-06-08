16ce868870fc8b0cae8cf96df8c8351e354db81c
viniquadros1997 <viniquadros1997@gmail.com>
2021-06-08 00:26:22 -0300

    findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -16,7 +17,14 @@ public class Program {
 		System.out.println("=== Test 1 : seller findById ===");
 		Seller seller = sellerdao.findById(3);
 		
-		System.out.println(seller);
+		System.out.println("=== Test 2 : seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerdao.findByDepartment(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 		
 		
 		
--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -2,6 +2,7 @@ package model.dao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -10,6 +11,8 @@ public interface SellerDao {
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findALL();
+	List<Seller> findByDepartment(Department department);
+	
 }
 
 
